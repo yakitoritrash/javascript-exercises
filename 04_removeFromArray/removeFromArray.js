ad1210fc0239c1fe55f201e426a1a4a7f87f22aa
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2024-08-14 14:05:20 +0530

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...element) {
+    const newArray = [];
+    array.forEach((item) => {
+        if (!element.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
