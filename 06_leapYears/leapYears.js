994afaaf6dad79bce097ba5f25990f0effcacdd7
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2024-09-03 17:12:38 +0530

    06 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivisiblebyfour = year % 4 === 0;
+    const isDivisiblebyhundred = year % 100 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
 
+
+    if (isDivisiblebyfour && 
+    (!isDivisiblebyhundred || isYearDivisibleByFourHundred)
+    )
+    {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
